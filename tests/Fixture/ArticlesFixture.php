1375d4d842b498fde8c55458ac4c8b1747b6abed
Bob <bobmulder96@gmail.com>
2015-03-04 18:42:51 +0100

    Added new Fixtures to test with

---
+++ tests/Fixture/ArticlesFixture.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Utils\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * Short description for class.
+ *
+ */
+class ArticlesFixture extends TestFixture
+{
+
+    /**
+     * fields property
+     *
+     * @var array
+     */
+    public $fields = array(
+        'id'                 => ['type' => 'integer'],
+        'user_id'            => ['type' => 'integer', 'null' => true],
+        'title'              => ['type' => 'string', 'null' => true],
+        'body'               => 'text',
+        'state'              => ['type' => 'integer', 'default' => 1],
+        'file_path'          => 'text',
+        'file_size'          => 'text',
+        'file_type'          => 'text',
+        'published'          => ['type' => 'string', 'length' => 1, 'default' => 'N'],
+        'created_by'         => ['type' => 'integer'],
+        'modified_by'        => ['type' => 'integer'],
+        'created_by_second'  => ['type' => 'integer'],
+        'modified_by_second' => ['type' => 'integer'],
+        '_constraints'       => ['primary' => ['type' => 'primary', 'columns' => ['id']]]
+    );
+
+    /**
+     * records property
+     *
+     * @var array
+     */
+    public $records = array(
+        array('user_id' => 1, 'state' => 1, 'title' => 'First Article', 'body' => 'First Article Body', 'published' => 'Y', 'created_by' => 1, 'modified_by' => 1),
+        array('user_id' => 3, 'state' => 0, 'title' => 'Second Article', 'body' => 'Second Article Body', 'published' => 'Y', 'created_by' => 1, 'modified_by' => 1),
+        array('user_id' => 1, 'state' => -1, 'title' => 'Third Article', 'body' => 'Third Article Body', 'published' => 'Y', 'created_by' => 1, 'modified_by' => 1)
+    );
+
+}
---
+++ tests/Fixture/RolesFixture.php
@@ -0,0 +1,62 @@
+<?php
+namespace Utils\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * RolesFixture
+ *
+ * Note: This Fixture is a clone of the UsersFixture of the CakeManager
+ * The clone is made to make the tests work without the CakeManager Plugin
+ */
+class RolesFixture extends TestFixture
+{
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'name' => ['type' => 'string', 'length' => 50, 'null' => false, 'default' => '0', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'login_redirect' => ['type' => 'string', 'length' => 256, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
+        'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => '0000-00-00 00:00:00', 'comment' => '', 'precision' => null],
+        'modified' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => '0000-00-00 00:00:00', 'comment' => '', 'precision' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+        ],
+        '_options' => [
+'engine' => 'InnoDB', 'collation' => 'latin1_swedish_ci'
+        ],
+    ];
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'id' => 1,
+            'name' => 'Administrators',
+            'login_redirect' => 'admin/manager/users',
+            'created' => '2014-12-30 13:22:01',
+            'modified' => '2014-12-30 13:22:01'
+        ],
+        [
+            'id' => 2,
+            'name' => 'Moderators',
+            'login_redirect' => '/',
+            'created' => '2014-12-30 13:22:01',
+            'modified' => '2014-12-30 13:22:01'
+        ],
+        [
+            'id' => 3,
+            'name' => 'Users',
+            'login_redirect' => '/',
+            'created' => '2014-12-30 13:22:01',
+            'modified' => '2014-12-30 13:22:01'
+        ],
+    ];
+}
